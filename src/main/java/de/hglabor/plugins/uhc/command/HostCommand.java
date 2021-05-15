97b79ee18cdea6f84f5ed3c4c8379a220498ece5
BestAutoyes <enrico-eifert@t-online.de>
2021-05-15 02:09:54 +0200

    updated hostcommand

--- src/main/java/de/hglabor/plugins/uhc/command/HostCommand.java
+++ src/main/java/de/hglabor/plugins/uhc/command/HostCommand.java
@@ -1,6 +1,8 @@
 package de.hglabor.plugins.uhc.command;
 
 import de.hglabor.plugins.uhc.config.ConfigInventory;
+import de.hglabor.plugins.uhc.game.GameManager;
+import de.hglabor.plugins.uhc.game.PhaseType;
 import dev.jorel.commandapi.CommandAPICommand;
 import org.bukkit.entity.Player;
 
@@ -8,6 +10,7 @@ public class HostCommand {
     public HostCommand() {
         new CommandAPICommand("host")
                 .withPermission("hglabor.forcestart")
+                .withRequirement((commandSender) -> GameManager.INSTANCE.getPhaseType().equals(PhaseType.LOBBY))
                 .withRequirement(commandSender -> commandSender instanceof Player)
                 .executesPlayer((player, objects) -> {
                     ConfigInventory.INSTANCE.openGUI(player);
