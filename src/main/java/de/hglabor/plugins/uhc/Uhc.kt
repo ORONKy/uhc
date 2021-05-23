0218b9a342ff0fabb0146eb1ad3fc55a6b62bbb4
BestAutoyes <enrico-eifert@t-online.de>
2021-05-23 22:22:32 +0200

    added helpop and killtop command

--- src/main/java/de/hglabor/plugins/uhc/Uhc.kt
+++ src/main/java/de/hglabor/plugins/uhc/Uhc.kt
@@ -103,6 +103,8 @@ class Uhc : KSpigot() {
         HostCommand()
         TeamCommand
         SendCoordsCommand
+        HelpOPCommand
+        KillTopCommand
     }
 
     private fun registerListener() {
