137f88396bf09667c2d286dd8f0b59a1cf2a5249
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-05-29 19:23:07 +0200

    Enabled HeartDisplay

--- src/main/java/de/hglabor/plugins/uhc/Uhc.kt
+++ src/main/java/de/hglabor/plugins/uhc/Uhc.kt
@@ -5,6 +5,7 @@ import de.hglabor.plugins.uhc.config.CKeys
 import de.hglabor.plugins.uhc.config.UHCConfig
 import de.hglabor.plugins.uhc.game.GameManager
 import de.hglabor.plugins.uhc.game.mechanics.GoldenHead
+import de.hglabor.plugins.uhc.game.mechanics.HeartDisplay
 import de.hglabor.plugins.uhc.game.mechanics.MobAIRemover
 import de.hglabor.plugins.uhc.game.mechanics.chat.GlobalChat
 import de.hglabor.plugins.uhc.game.scenarios.*
@@ -112,6 +113,7 @@ class Uhc : KSpigot() {
         pluginManager.registerEvents(ScoreboardManager.INSTANCE, this)
         pluginManager.registerEvents(GoldenHead.INSTANCE, this)
         pluginManager.registerEvents(GlobalChat.INSTANCE, this)
+        pluginManager.registerEvents(HeartDisplay.INSTANCE, this)
         pluginManager.registerEvents(MobAIRemover(), this)
     }
 }
